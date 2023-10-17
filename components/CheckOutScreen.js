af9c8e51ecf2f8052f28acbdddb7b2394a436dfe
Loy3 <netshiozwiloy@gmail.com>
2023-10-17 10:17:50 +0200

    17/10/2023

--- components/CheckOutScreen.js
+++ components/CheckOutScreen.js
@@ -199,7 +199,7 @@ const CheckOutScreen = ({ navigation }) => {
     }
 
     async function confirmPament() {
-        const convAmount = Math.floor((parseInt(itemsTotalPrice) * 0.053) * 100);
+        const convAmount = Math.floor(parseInt(itemsTotalPrice) * 100);
         console.log(convAmount);
         const myAmount = {
             amount: convAmount
@@ -319,10 +319,10 @@ user id,
         setalertStatus(false);
     }
 
-    function convertToUsd() {
-        const convAmount = Math.floor((parseInt(itemsTotalPrice) * 0.053));
-        return convAmount;
-    }
+    // function convertToUsd() {
+    //     const convAmount = Math.floor((parseInt(itemsTotalPrice) * 0.053));
+    //     return convAmount;
+    // }
 
 
     if (loadingStatus === true) {
@@ -447,10 +447,10 @@ user id,
                                 <Text style={styles.totalLeft}>Total</Text>
                                 <Text style={styles.totalRight}>R{itemsTotalPrice}.00</Text>
                             </View>
-                            <View style={{ flexDirection: "row" }}>
+                            {/* <View style={{ flexDirection: "row" }}>
                                 <Text style={styles.totalLeft}>Total in USD</Text>
                                 <Text style={styles.totalRight}>${convertToUsd()}.00</Text>
-                            </View>
+                            </View> */}
 
 
                         </View>
@@ -498,8 +498,8 @@ user id,
                 <View style={{ width: "100%", height: "100%", zIndex: 99, backgroundColor: "rgba(0,0,0,0.5)", position: "absolute", top: 0, left: 0, justifyContent: "center", alignItems: "center" }}>
                     <View style={{ width: "90%", height: "35%", backgroundColor: "#FFFEF5", borderRadius: 20, alignItems: "center", justifyContent: "center" }}>
                         <View style={{ alignItems: "center", justifyContent: "center", width: "100%" }}>
-                            <Text style={[styles.modalTitle, { margin: 0, fontSize:24 }]}>{alertMessageHead}</Text>
-                            <Text style={[styles.modalTitle, { marginVertical: 8, textAlign:"center", fontSize:16 }]}>{alertMessage}</Text>
+                            <Text style={[styles.modalTitle, { margin: 0, fontSize: 24 }]}>{alertMessageHead}</Text>
+                            <Text style={[styles.modalTitle, { marginVertical: 8, textAlign: "center", fontSize: 16 }]}>{alertMessage}</Text>
 
                             <TouchableOpacity style={{ height: 50, backgroundColor: "#7C9070", marginTop: 10, width: "70%", borderRadius: 50 }} onPress={closeAlert}>
                                 <Text style={styles.siBtnTxt}>Close</Text>
@@ -692,7 +692,7 @@ const styles = StyleSheet.create({
 
     pricingCont: {
         width: "90%",
-        height: 250,
+        height: 200,
         marginHorizontal: "5%",
         // backgroundColor: "yellow",
         marginTop: 100,
